979d1284bf5654496f1449b17cab00489e94f7b1
code-shubhambhatt <shubhambhatt1000@gmail.com>
2026-09-09 11:39:19 +0530

    Add project database model

---
+++ models/project.py
@@ -0,0 +1,17 @@
+from sqlalchemy import ARRAY, String, Boolean, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    short_description: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    github_url: Mapped[str] = mapped_column(String, nullable=False)
+    demo_url: Mapped[str] = mapped_column(String, nullable=True)
+    featured: Mapped[str] = mapped_column(Boolean, nullable=False)
+    technologies: Mapped[list: Mapped[str]] = mapped_column(ARRAY(String), nullable=False)
+    display_order: Mapped[int] = mapped_column(Integer, nullable=False)
