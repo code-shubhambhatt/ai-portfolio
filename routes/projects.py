85a98ce65861e600212507aecd81ec898c477d7c
code-shubhambhatt <shubhambhatt1000@gmail.com>
2026-09-09 11:39:41 +0530

    Connect projects API to database

--- routes/projects.py
+++ routes/projects.py
@@ -1,47 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
 from schemas.projects import Project_Response
+from models.project import Project
+from database  import get_db
+
 
 router = APIRouter(
     prefix="/api",
     tags=["portfolio"]
 )
 
-projects = [
-    {
-        "id": 1,
-        "name": "Astro",
-        "short_description": "...",
-        "description": "...",
-        "github_url": "https://github.com/code-shubhambhatt/astro",
-        "demo_url": "https://astro-39u.pages.dev/",
-        "featured": True,
-        "technologies": ["Python", "FastAPI"],
-        "display_order": 1
-    },
-    {
-        "id": 2,
-        "name": "My Blog Application",
-        "short_description": "...",
-        "description": "...",
-        "github_url": "https://github.com/code-shubhambhatt/myblogapp",
-        "demo_url": None,
-        "featured": False,
-        "technologies": ["Python", "Flask", "MongoDB"],
-        "display_order": 2
-    },
-    {
-        "id": 3,
-        "name": "Watchmate API",
-        "short_description": "...",
-        "description": "...",
-        "github_url": "https://github.com/code-shubhambhatt/drf",
-        "demo_url": None,
-        "featured": False,
-        "technologies": ["Python", "Django", "DRF"],
-        "display_order": 3
-    }
-]
+
 
 @router.get("/projects", response_model= list[Project_Response])
-def get_projects():
-    return projects
+def get_projects(db:Session=Depends(get_db)):
+    result = db.execute(select(Project))
+    results = result.scalars().all()
+    print(results)
+    return results
