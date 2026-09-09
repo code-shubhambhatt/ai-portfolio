8551e6f03b24a66114911ce3da22891325e5f93b
code-shubhambhatt <shubhambhatt1000@gmail.com>
2026-09-09 11:50:56 +0530

    Add project detail endpoint

--- routes/projects.py
+++ routes/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
@@ -6,17 +6,25 @@ from schemas.projects import Project_Response
 from models.project import Project
 from database  import get_db
 
-
 router = APIRouter(
     prefix="/api",
     tags=["portfolio"]
 )
 
-
-
 @router.get("/projects", response_model= list[Project_Response])
 def get_projects(db:Session=Depends(get_db)):
     result = db.execute(select(Project))
-    results = result.scalars().all()
-    print(results)
-    return results
+    projects = result.scalars().all()
+    return projects
+
+@router.get("/projects/{project_id}", response_model=Project_Response)
+def get_project_details(project_id:int, db:Session = Depends(get_db)):
+    result = db.execute(
+        select(Project).where(Project.id==project_id)
+    )
+    project = result.scalar_one_or_none()
+
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found!")
+    
+    return project
